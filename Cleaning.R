d9c4b1125bd85c2e685b6a6538b229f06b3b896e
unknown <khekit@hotmail.com>
2021-04-26 13:37:16 -0500

    add file

---
+++ Cleaning.R
@@ -0,0 +1,14 @@
+rm(list = ls()) # clean global environment
+# load packages:
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+library(ggplot2)
+library(pROC)
+library(RColorBrewer)
+
+# read in customer churn data
+mara <- read.csv("D:/Drake Spring 2021/Drake STAT172/Final_Project/MarathonData.csv", stringsAsFactors = TRUE)
+summary(mara)
+
+unique(mara$Wall21)
