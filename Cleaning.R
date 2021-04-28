f09c5d2f2622c1db5e33c0451866863c3e5b6f55
sammoon99 <samantha.moon@drake.edu>
2021-04-28 13:03:17 -0500

    Update Cleaning.R

--- Cleaning.R
+++ Cleaning.R
@@ -8,17 +8,10 @@ library(pROC)
 library(RColorBrewer)
 
 # read in customer churn data
-mara <- read.csv(choose.files(), header = T, stringsAsFactors = TRUE)
-# for Mac
 mara <- read.csv(file.choose(), header = T, stringsAsFactors = TRUE)
 View(mara)
 summary(mara)
 
 unique(mara$Wall21)
 
-#
-mara$speed <- mara$CATEGORY
-mara$speed[mara$CATEGORY == "A"] <- "Fast"
-mara$speed[mara$CATEGORY == "B"] <- "Fast"
-mara$speed[mara$CATEGORY == "C"] <- "Slow"
-mara$speed[mara$CATEGORY == "D"] <- "Slow"
+mara$speed <- ifelse(mara$CATEGORY == "A" | mara$CATEGORY == "B", "Fast", "Slow")
