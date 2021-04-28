5f47d7fe94309d306e967744a12bf00cdce3f319
sammoon99 <samantha.moon@drake.edu>
2021-04-28 13:26:53 -0500

    Update Cleaning.R

--- Cleaning.R
+++ Cleaning.R
@@ -20,3 +20,6 @@ mara$speed <- ifelse(mara$CATEGORY == "A" | mara$CATEGORY == "B", "Fast", "Slow"
 
 # Make a binary cross training variable
 mara$CrossTraining_bin <- ifelse(mara$CrossTraining == "", "No", "Yes")
+
+# Fixing the piece of data in sp4week that had "11125.00000"
+mara$sp4week[82] <- 11.125
